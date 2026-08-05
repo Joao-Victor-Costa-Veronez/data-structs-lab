0bb755c91c2ba8d6191319458270f8f77a7198a1
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-05 10:11:20 -0300

    feat: add menu

--- college_classes/exercises/function_exercises_2/program.c
+++ college_classes/exercises/function_exercises_2/program.c
@@ -8,13 +8,18 @@ DATE: 08/05/2026
 #include <string.h>
 
 // Declaring functions
+void showMainMenu();
 void showVectorsIntegers(int vector[], int length);
 
 // Main function
 int main() {
     // Declaring variables
+    int option = 0;
     int length = 10;
     int vector[length];
+
+    // Title
+    printf("+=====          FUNCTIONS   -   EXERCISE    2      =====+\n\n");
 
     // For each element in the vector
     for (int i = 0; i < length; i++) {
@@ -23,10 +28,40 @@ int main() {
         scanf("%d", &vector[i]);
     }
 
-    // Show the integers of the element
-    showVectorIntegers(vector, length);
+    // Do
+    do {
+        // Showing the menu
+        showMainMenu();
+        // Getting the chose option
+        scanf("%d", &option);
+
+        // In the case of the option be
+        switch (option) {
+        // 1
+        case 1: // Show the integers of the element
+                showVectorIntegers(vector, length);
+
+                break;
+
+        // 2
+        case 2:
+
+        }
+      // While option is different of 8
+    } while (option != 6);
 
     return 0;
+}
+
+// Function that shows the temperature convertions menu
+void showTemperatureConvertionsMenu() {
+    printf("+=======================================================+\n");
+    printf("|           OPERATIONS AND VERIFICATIONS MENU           |\n");
+    printf("+=======================================================+\n");
+    printf("|   1. Show the integers of a vector.                   |\n");
+    printf("|   2.                         |\n");
+    printf("+=======================================================+\n");
+    printf("|   Choose your operation: ");
 }
 
 // Funtion that shows the integers of a vector
