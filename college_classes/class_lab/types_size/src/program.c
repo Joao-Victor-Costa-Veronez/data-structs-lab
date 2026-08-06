7348686815e1b2058e60b634b2febb74c3f9745c
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-06 10:12:21 -0300

    feat: add test of a size of a struct

--- college_classes/class_lab/types_size/src/program.c
+++ college_classes/class_lab/types_size/src/program.c
@@ -1,15 +1,28 @@
+/*
+DATE: 06/08/2026
+*/
+
+// Importing libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Declaring structs
+struct point2D {
+    int x;
+    int y;
+};
+
+// Main function
 int main() {
-    printf("%d\n", sizeof(int));
-    printf("%d\n", sizeof(char));
-    printf("%d\n", sizeof(float));
-    printf("%d\n", sizeof(int[10]));
-    printf("%d\n", sizeof(char[10]));
-    printf("%d\n", sizeof(float[10]));
-    printf("%d\n", sizeof(int));
-    
+    // Showing the sizes of the types
+    printf("%ld\n", sizeof(int));    // 4
+    printf("%ld\n", sizeof(char));   // 1
+    printf("%ld\n", sizeof(float));  // 4
+    printf("%ld\n", sizeof(int[10]));    // 40
+    printf("%ld\n", sizeof(char[10]));   // 10
+    printf("%ld\n", sizeof(float[10]));  // 40
+    printf("%ld\n", sizeof(struct point2D));  // 8
+
     return 0;
 }
