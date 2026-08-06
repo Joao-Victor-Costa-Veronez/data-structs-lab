b1620b53272588bb02875d9e02b6bf7ff1d9e00b
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-06 10:42:24 -0300

    feat: add phase 1 of struct exercise

--- college_classes/exercises/struct_exercises/src/program.c
+++ college_classes/exercises/struct_exercises/src/program.c
@@ -7,8 +7,18 @@ DATE: 06/08/2026
 #include <stdlib.h>
 #include <string.h>
 
+// Declaring structs
+typedef struct {
+    char name[100];
+    char address[100];
+    float averagePrice;
+    char foodType[100];
+} restaurant;
+
 // Main function
 int main() {
-    
+    // Declaring variables
+    restaurant unitedBurgers;
+
     return 0;
 }
