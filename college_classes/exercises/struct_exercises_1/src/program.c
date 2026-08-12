223123fe6dc71f51d146490cc646a7335547bff3
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-12 10:10:59 -0300

    feat: add functios to show a vectro os restaurants and show restaurants that are in a range of prices

--- college_classes/exercises/struct_exercises_1/src/program.c
+++ college_classes/exercises/struct_exercises_1/src/program.c
@@ -16,17 +16,19 @@ typedef struct {
 // Including libraries
 restaurant createRestaurant();
 void showRestaurant(restaurant r);
+void showRestaurants(restaurant restaurants[], int quantity);
+void showRestaurantsPriceRange(restaurant restaurants[], int quantity, float minPrice, float maxPrice);
+void preguica(restaurant restaurantes[]);
 
 // Main function
 int main() {
     // Declaring variables
-    restaurant unitedBurgers;
+    restaurant restaurants[10];
 
-    // Creating a restaurant
-    unitedBurgers = createRestaurant();
+    preguica(restaurants);
 
     // Showing the restaurant
-    showRestaurant(unitedBurgers);
+    showRestaurants(restaurants, 3);
 
     return 0;
 }
@@ -58,4 +60,38 @@ void showRestaurant(restaurant r) {
     printf("Address: %s\n", r.address);
     printf("Average price: $ %.2f\n", r.averagePrice);
     printf("Food type: %s\n", r.foodType);
+}
+
+// Function that shows retaurants
+void showRestaurants(restaurant restaurants[], int quantity) {
+    for (int i = 0; i < quantity; i++) {
+        showRestaurant(restaurants[i]);
+    }
+}
+
+// Function that shows the restaurants in the range of prices
+void showRestaurantsPriceRange(restaurant restaurants[], int quantity, float minPrice, float maxPrice) {
+    for (int i = 0; i < quantity; i++) {
+        if (restaurants[i].averagePrice <= minPrice && restaurants[i].averagePrice <= maxPrice) {
+            showRestaurant(restaurants[i]);
+        }
+    }
+}
+
+void preguica(restaurant restaurantes[])
+{
+    strcpy(restaurantes[0].name, "McDonalds");
+    strcpy(restaurantes[0].address, "Rua Seilah, 222");
+    restaurantes[0].averagePrice = 50.0;
+    strcpy(restaurantes[0].foodType, "Fast Food");
+
+    strcpy(restaurantes[1].name, "Habibs");
+    strcpy(restaurantes[1].address, "Rua Seilah, 333");
+    restaurantes[1].averagePrice = 20.0;
+    strcpy(restaurantes[1].foodType, "Arabe");
+
+    strcpy(restaurantes[2].name, "Burger King");
+    strcpy(restaurantes[2].address, "Rua Seilah, 444");
+    restaurantes[2].averagePrice = 50.0;
+    strcpy(restaurantes[2].foodType, "Fast Food");
 }
