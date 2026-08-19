94e009c66691ac841538b7c86dcf962363e3b13c
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-19 11:26:25 -0300

    feat: add exercise 1 of pointers exercises

--- college_classes/exercises/pointers_and_memory_allocation/src/programa.c
+++ college_classes/exercises/pointers_and_memory_allocation/src/programa.c
@@ -1,12 +1,28 @@
 /*
 DATE: 08/19/2022
 */
+/*
 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
+float temperatureConversion(float *temperature, int *option);
+int main()
+{
     printf("Funciona!\n");
     return 0;
+}
+
+float temperatureConversion(float *temperature, int *option)
+{
+    if (*option == 0)
+    {
+        return *temperature * 1.8 + 32;
+    }
+    else if (*option == 1)
+    {
+        return 5 / 9 * (*temperature - 32);
+    }
 }
