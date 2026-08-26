dd48e3ae18981462b24ae62d8a759c95e8c99a59
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-26 10:38:46 -0300

    feat: add phase 10 of pointers exercise complete

--- college_classes/exercises/pointers_and_memory_allocation/src/programa.c
+++ college_classes/exercises/pointers_and_memory_allocation/src/programa.c
@@ -6,6 +6,7 @@ DATE: 08/19/2022
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "utilidades.h"
 
 float temperatureConversion(float *temperature, int *option);
 void showVector(int *vector, int lenght);
@@ -18,6 +19,64 @@ int searchValueInVector(int *vector, int length, int a);
 int checkOrdination(int *vector, int length);
 int main()
 {
+    int length, *pointer;
+    int greatest, lowest;
+    int value, position;
+
+    printf("What is the length of the vector: ");
+    length = lerInt();
+
+    pointer = (int *)malloc(sizeof(int) * length);
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Inform [%d]: ", i);
+        *(pointer + i) = lerInt();
+    }
+
+    srand((unsigned int)time(NULL));
+    for (int i = 10; i < 20; i++)
+    {
+        *(pointer + i) = (rand() % (100 - 1 + 1)) + 1;
+    }
+
+    showVector(pointer, length);
+
+    printf("The average value of the vector is: %.2f\n\n", averageVectorValue(pointer, length));
+
+    greatest = findGreatestVectorValueIndex(pointer, length);
+    printf("The index of the greatest number of the vector is: %d\n\n", greatest);
+    lowest = findLowestVectorValueIndex(pointer, length);
+    printf("The index of the lowest number of the vector is: %d\n\n", lowest);
+
+    printf("Inform a value to search for: ");
+    value = lerInt();
+    position = searchValueInVector(pointer, length, value);
+
+    if (position)
+    {
+        printf("The value is in the position: %d\n\n", position);
+    }
+    else
+    {
+        printf("The value was not found in the vector.\n\n");
+    }
+
+    switch (checkOrdination(pointer, length))
+    {
+    case 0:
+        printf("The vector is disorderly.\n\n");
+    case 1:
+        printf("The vector is descending.\n\n");
+    case 2:
+        printf("The vector is ascending.\n\n");
+    }
+
+    free(pointer);
+
+    return 0;
+
+    /* TESTS
     float temperature = 26.0;
     int option = 0;
     int length = 5, vector[length];
@@ -55,7 +114,8 @@ int main()
 
     printf("The ordination of the vector is (0: disorderly; 1: ascending; 2: descending): %d\n\n", checkOrdination(vector, length));
 
-    return 0;
+    return 0
+    */
 }
 
 float temperatureConversion(float *temperature, int *option)
@@ -135,12 +195,16 @@ int findLowestVectorValueIndex(int *vector, int length)
     return lowestValueIndex;
 }
 
+// Function that check for repeated values in the vector
 int checkRepeatedValues(int *vector, int length)
 {
+    // For each element in the vector
     for (int i = 0; i < length; i++)
     {
+        // For each value after the element I'm comparing with the others
         for (int j = i; j < length; j++)
         {
+            // If the
             if ((vector[i] == vector[j]) && (i != j))
             {
                 return 1;
@@ -151,6 +215,7 @@ int checkRepeatedValues(int *vector, int length)
     return 0;
 }
 
+// Function that search for a value in the vector
 int searchValueInVector(int *vector, int length, int a)
 {
     for (int i = 0; i < length; i++)
@@ -164,8 +229,10 @@ int searchValueInVector(int *vector, int length, int a)
     return -1;
 }
 
+// Function that checks the ordination of a vector
 int checkOrdination(int *vector, int length)
 {
+    // Declaring variables
     int ascending = 0, descending = 0;
 
     for (int i = 1; i < length; i++, vector++)
