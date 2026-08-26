c75c107b0280d2c09e576a874c7906cd84d0f57c
Joao Victor <jcostaveronez@gmail.com>
2026-08-25 21:12:45 -0300

    feat: add phase 5 of the pointer exercise complete

--- college_classes/exercises/pointers_and_memory_allocation/src/programa.c
+++ college_classes/exercises/pointers_and_memory_allocation/src/programa.c
@@ -10,6 +10,7 @@ float temperatureConversion(float *temperature, int *option);
 void showVector(int *vector, int lenght);
 void randomizeVector(int *vector, int length, int min, int max);
 float averageVectorValue(int *vector, int length);
+int findGreatestVectorValue(int *vector, int length);
 int main()
 {
     float temperature = 26.0;
@@ -35,6 +36,8 @@ int main()
     randomizeVector(vector, length, min, max);
 
     printf("The average value of the vector is: %.2f\n\n", averageVectorValue(vector, length));
+
+    printf("The greatest number of the vector is: %d\n\n", findGreatestVectorValue(vector, length));
 
     return 0;
 }
@@ -87,4 +90,18 @@ float averageVectorValue(int *vector, int length)
     }
 
     return sum / length;
+}
+
+int findGreatestVectorValue(int *vector, int length)
+{
+    int greatest = *vector;
+    for (int i = 0; i < length; i++)
+    {
+        if (greatest < *(vector + i))
+        {
+            greatest = *(vector + i);
+        }
+    }
+
+    return greatest;
 }
