a466f438ad2954d1aa309581258c4600703497fd
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-27 11:14:45 -0300

    feat: implementing an inicial list

--- college_classes/class_lab/project_list_simply_connected/src/programa.c
+++ college_classes/class_lab/project_list_simply_connected/src/programa.c
@@ -6,13 +6,16 @@
 
 int main()
 {
-    node_SLL *n1 = create_node_SLL(10, NULL);
-    node_SLL *n2 = create_node_SLL(20, n1);
-    node_SLL *n3 = create_node_SLL(30, n2);
+    list_SLL *list = create_list_SLL();
 
-    list_SLL *list = (list_SLL *)malloc(sizeof(list_SLL));
-    list->inicial = n3;
-    list->length = 3;
+    list->inicial = create_node_LSC(10, NULL); 
+    list->length++;
+
+    list->inicial->next = create_node_LSC(20, NULL);
+    list->length++;
+
+    list->inicial->next->next = create_node_LSC(30, NULL);
+    list->length++;
 
     return 0;
 }
