9c178fd50b488fda55ce0358bef32a723db7a764
João Victor Costa Veronez <jcostaveronez@gmail.com>
2026-08-27 10:09:12 -0300

    feat: add new project of list simply connected

---
+++ college_classes/class_lab/project_list_simply_connected/include/utilidades.h
@@ -0,0 +1,12 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void lerString(char texto[], int tamanho);
+int lerInt();
+float lerFloat();
+
+#endif
